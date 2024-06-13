c9a4772a41c4e58e1d1af59552eb33f25c956347
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2024-06-12 21:59:29 -0600

    changed the styling for the images, also added the contact me oage and the contact me styling and linckage with css.

--- santos_portfolio/src/components/Contact.js
+++ santos_portfolio/src/components/Contact.js
@@ -1,11 +1,21 @@
 // src/components/Contact.js
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
     return (
         <section id="contact">
-            <h2>Contact Me</h2>
-            <p>This is the contact me section of your web page.</p>
+           <div class = "contact-title">
+            <p>For more information fell free to Contact me!</p>
+            </div>
+            <div class = "background">
+            <div class = "contact-forms">
+                <p><b>Email:</b> Johnfernando1910@gmail.com </p>
+                <p><b>Phone:</b> +1 (825) 925 0834</p>
+                <p><b>LinkedIn:</b> <a href = "https://www.linkedin.com/in/juan-fernando-santos-59352729b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"> Click here!</a></p>
+
+            </div>
+            </div>
         </section>
     );
 };
