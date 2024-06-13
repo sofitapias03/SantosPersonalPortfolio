66501a3560ea713a30c8254a68823a76bbc97996
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2024-06-12 22:21:41 -0600

    added text and title and some styling to the research interest section, request sent for the investigation paper to coordinator.

--- santos_portfolio/src/components/Research.js
+++ santos_portfolio/src/components/Research.js
@@ -8,6 +8,17 @@ const Research = () => {
             <div class = "research-title">
                 Research Interest:
             </div>
+            <br/>
+            <br/>
+            <p class = "research-text">
+            I have always been interested in research involving infectious diseases, focusing on the complex mechanisms that take place during an 
+            immune response to protect our bodies. During highschool, I selected my Extended essay topic to explore the genetic mutations and 
+            determinants involved during HIV infections. I looked at the genetic composition of the virus, the mechanisms via which it accesses, 
+            infects and replicates within cells, and suggested a correlation between the length of genetic code and the number of infections related 
+            to this. HIV sparked my interest, as it is commonly referred to as the “perfect virus”, targeting the cells meant to protect us, and thus 
+            I am interested in continuing my research and expanding my knowledge in this type of infectious diseases at a molecular level. 
+
+            </p>
         </section>
     );
 };
